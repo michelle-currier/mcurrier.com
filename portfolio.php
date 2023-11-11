2fb72cb0ca08b38613999d0a390cee36a40bc6b2
michelle currier <michellecurrier@michelles-MacBook-Pro.local>
2023-11-10 19:20:00 -0600

    updates to the image links and css hover on langs

--- portfolio.php
+++ portfolio.php
@@ -21,7 +21,7 @@
 					<div class="slideContainer">
 						<div class="slider-wrapper">
 						<div class="inner-wrapper">
-							<div class="slide"><img src="https://www.djnicerack.com/images/portfolio/party_email.jpg"></div>
+							<div class="slide"><img src="http://mcurrier.com/images/party_email.jpg"></div>
 							<div class="slide"><img src="http://mcurrier.com/images/essentialCBDLogo.png"></div>
 							<div class="slide"><img src="http://mcurrier.com/images/tracking_adv.jpg"></div>		
 							<div class="slide"><img src="http://mcurrier.com/images/url_breakdown.jpg"></div>
@@ -30,14 +30,14 @@
 							<div class="slide"><img src="http://mcurrier.com/images/get_social.jpg"></div>
 							<div class="slide"><img src="http://mcurrier.com/images/affiliate_link.jpg"></div>
 							<div class="slide"><img src="http://mcurrier.com/images/powderpuff.jpg"></div>							
-							<div class="slide"><img src="https://www.djnicerack.com/images/portfolio/savethedate2.jpg"></div>
-							<div class="slide"><img src="https://www.djnicerack.com/images/portfolio/bees_knees.png"></div>
-							<div class="slide"><img src="https://www.djnicerack.com/images/portfolio/tournament_email.jpg"></div>
-							<div class="slide"><img src="https://www.djnicerack.com/images/portfolio/flyer_email-10-22.jpg"></div>
-							<div class="slide"><img src="https://www.djnicerack.com/images/portfolio/snow-cuts.jpg"></div>
-							<div class="slide"><img src="https://www.djnicerack.com/images/portfolio/EtcAug14.jpg"></div>
-							<div class="slide"><img src="https://www.djnicerack.com/images/portfolio/bloom_daria_alldays.jpg"></div>
-							<div class="slide"><img src="https://www.djnicerack.com/images/portfolio/ig_deepcuts.jpg"></div>
+							<div class="slide"><img src="http://mcurrier.com/images/savethedate2.jpg"></div>
+							<div class="slide"><img src="http://mcurrier.com/images/bees_knees.png"></div>
+							<div class="slide"><img src="http://mcurrier.com/images/tournament_email.jpg"></div>
+							<div class="slide"><img src="http://mcurrier.com/images/flyer_email-10-22.jpg"></div>
+							<div class="slide"><img src="http://mcurrier.com/images/snow-cuts.jpg"></div>
+							<div class="slide"><img src="http://mcurrier.com/images/EtcAug14.jpg"></div>
+							<div class="slide"><img src="http://mcurrier.com/images/bloom_daria_alldays.jpg"></div>
+							<div class="slide"><img src="http://mcurrier.com/images/ig_deepcuts.jpg"></div>
 						</div>
 						</div>
 
